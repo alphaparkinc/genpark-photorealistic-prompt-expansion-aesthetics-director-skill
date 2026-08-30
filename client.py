3ca9e23f05415206a38ed79c458864abf2235e64
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-08-30 11:34:50 +0800

    feat: add client.py

---
+++ client.py
@@ -0,0 +1,9 @@
+class PhotorealisticPromptExpansionAestheticsDirectorClient:
+    def expand_aesthetics_prompt(self, raw_creative_idea='Cyberpunk street food vendor in rain', camera_sensor_profile='HASSELBLAD_MEDIUM_FORMAT_100MP'):
+        return {
+            'director_session_id': 'aes_dir_8812',
+            'sensor_profile': camera_sensor_profile,
+            'expanded_cinematic_prompt': 'Cinematic medium shot of an elderly street noodle chef illuminated by neon holographic menus, raindrops refracting ambient cyan-magenta light, 85mm f/1.4 lens, volumetric steam, hyper-detailed skin pores, Kodachrome color grading, award-winning editorial photography',
+            'lighting_and_volumetrics_score_pct': 99.6,
+            'photorealism_index_gain_pct': 48.2
+        }
